c191137fe36d7de0be55a700ba8413c0175f7767
sethaman <97506784+sethaman@users.noreply.github.com>
2022-02-01 17:11:35 +0530

    Initialize dependabot/npm_and_yarn/hosted-git-info-2.8.9

---
+++ src/components/ComponentSwitch.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Button from './Button'
+    import RichTextBlock from './RichTextBlock'
+    import MarkdownBlock from './MarkdownBlock'
+
+    export let componentData
+
+    let COMPONENT_LIST = [
+        { id: 'button', component: Button },
+        { id: 'richTextBlock', component: RichTextBlock },
+        { id: 'markdownBlock', component: MarkdownBlock }
+    ]
+
+    const selectedComponent = COMPONENT_LIST.filter((item) => item.id === componentData.type)[0]
+</script>
+
+{#if selectedComponent && selectedComponent.component}
+    <svelte:component this={selectedComponent.component} props={componentData.fields} />
+{/if}
