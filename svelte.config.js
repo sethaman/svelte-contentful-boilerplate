1c3a61b9e747e3a7f0db1c1f7d9a471b4fc5888c
sethaman <97506784+sethaman@users.noreply.github.com>
2022-02-01 17:11:33 +0530

    Initialize snyk-fix-3d5e1e8ed65476a5caedfd87dcdd733d

---
+++ svelte.config.js
@@ -0,0 +1,8 @@
+const sveltePreprocess = require('svelte-preprocess')
+
+module.exports.preprocess = sveltePreprocess({
+    postcss: {
+        plugins: [require('tailwindcss'), require('autoprefixer')]
+    },
+    scss: true
+})
