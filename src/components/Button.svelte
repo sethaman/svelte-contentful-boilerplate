9cc52e69d2e7f57fcbd4bbad9720fc0ff0ed6a93
sethaman <97506784+sethaman@users.noreply.github.com>
2022-02-01 17:11:38 +0530

    Initialize dependabot/npm_and_yarn/path-parse-1.0.7

---
+++ src/components/Button.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let props
+    const { title, url } = props
+</script>
+
+<style>
+    .btn {
+        padding: 10px;
+        margin: 10px;
+    }
+</style>
+
+<a class="btn bg-green-500 text-white font-bold" href={url}>{title}</a>
---
+++ src/components/RichTextBlock.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+
+    export let props
+    const { text } = props
+</script>
+
+{@html documentToHtmlString(text)}
